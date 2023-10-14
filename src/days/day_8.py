ade06b3c63de83f722e987f609dbe53398774073
coldhearti <eemil.kiviahde@gmail.com>
2023-10-14 20:45:24 +0300

    Solved day 8 part 1, started on part 2

--- src/days/day_8.py
+++ src/days/day_8.py
@@ -1,11 +1,45 @@
-from exc import not_implemented
+from typing import List
+from exc import under_construction
+import numpy as np
 
 
-@not_implemented
+def check_visibility(row: np.ndarray):
+    highest = -1
+    visible = np.zeros(row.shape, dtype=bool)
+    for idx, el in enumerate(row):
+        if el > highest:
+            highest = el
+            visible[idx] = True
+    return visible
+
+
+def parse_map(lines):
+    rows = [[int(char) for char in line.strip(" \n")] for line in lines]
+    return np.array(rows, dtype=int)
+
+
 def solve_part_1(input_path: str):
-    ...
+    map: np.ndarray
+    with open(input_path, mode="r") as fp:
+        map = parse_map(fp.readlines())
+    visible_map = np.zeros(map.shape, dtype=bool)
+    for row_idx, row in enumerate(map):
+        left = check_visibility(row)
+        right = np.flip(check_visibility(np.flip(row)))
+        visible_map[row_idx] = np.logical_or(left, right)
+
+    for col_idx, col in enumerate(map.T):
+        left = check_visibility(col)
+        right = np.flip(check_visibility(np.flip(col)))
+
+        visible = np.logical_or(left, right)
+        visible_map.T[col_idx] = np.logical_or(visible_map.T[col_idx], visible)
+
+    return visible_map.sum()
 
 
-@not_implemented
+@under_construction
 def solve_part_2(input_path: str):
-    ...
+    map: np.ndarray
+    with open(input_path, mode="r") as fp:
+        map = parse_map(fp.readlines())
